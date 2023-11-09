1b4bb124a4573ea0ab27825126c9e14d18af12ad
aliromero <aliyeketaz@gmail.com>
2023-11-10 03:00:20 +0330

    update

--- example/android/app/src/main/kotlin/com/github/chinloyal/pusher_client_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/github/chinloyal/pusher_client_example/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.github.chinloyal.pusher_client_example
 
 import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterActivity() {
 }
